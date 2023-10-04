db08d486110f2432d5786169e34ebae96b500990
rami2076 <sakusennx@gmail.com>
2023-10-04 14:05:07 +0900

    nameをfull_nameに修正/フォーマットを変更

--- datasource/init.sql
+++ datasource/init.sql
@@ -1,9 +1,11 @@
 use mydatabase;
 CREATE TABLE EMPLOYEE
 (
-    EMPLOYEE_NUMBER   int(10) unsigned auto_increment primary key COMMENT '社員番号は0-9999999999までの数値とする．',
-    NAME  varchar(100) NOT NULL COMMENT '氏名:推奨値は20だが,100まで許容する.許容文字については別途検討する．',
-    AGE TINYINT(3) unsigned NOT NULL COMMENT '年齢:0-200歳までをサービス利用者として想定しその間を許容文字数とする．',
-    EMAIL_ADDRESS   varchar(255) NOT NULL COMMENT 'メールアドレス:RFC5321より10文字以上-254文字以内を許容文字数とする.許容文字については別途検討する.',
-    INDEX index_name_employee(EMPLOYEE_NUMBER)
-)ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+    EMPLOYEE_NUMBER int(10) unsigned auto_increment primary key COMMENT '社員番号は0-9999999999までの数値とする．',
+    FULL_NAME       varchar(100)        NOT NULL COMMENT '氏名:推奨値は20だが,100まで許容する.許容文字については別途検討する．',
+    AGE             TINYINT(3) unsigned NOT NULL COMMENT '年齢:0-200歳までをサービス利用者として想定しその間を許容文字数とする．',
+    EMAIL_ADDRESS   varchar(255)        NOT NULL COMMENT 'メールアドレス:RFC5321より10文字以上-254文字以内を許容文字数とする.許容文字については別途検討する.',
+    INDEX index_name_employee (EMPLOYEE_NUMBER)
+) ENGINE = InnoDB
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_bin;
