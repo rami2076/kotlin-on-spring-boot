eec4a3bd63af05bed2f7acb0cd4fbd7cb02a1e64
nyx <sakusennx@gmail.com>
2023-10-05 04:47:15 +0900

    validが動作していないので検証

--- src/main/kotlin/com/example/kotlinonspringboot/presentation/controller/EmployeeSearchController.kt
+++ src/main/kotlin/com/example/kotlinonspringboot/presentation/controller/EmployeeSearchController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
 
 @RestController
+//@Validated
 class EmployeeSearchController : EmployeeSearchApi {
 
     override fun list(): ResponseEntity<EmployeeSearchResponse> {
