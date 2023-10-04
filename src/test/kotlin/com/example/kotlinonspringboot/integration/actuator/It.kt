6bc45d493dbff6c9f7cdd0c8598340ddedcbf0a2
nyx <sakusennx@gmail.com>
2023-10-05 03:04:35 +0900

    検証:Actionsでのみ失敗する試験の名称を変更

--- src/test/kotlin/com/example/kotlinonspringboot/integration/actuator/It.kt
+++ src/test/kotlin/com/example/kotlinonspringboot/integration/actuator/It.kt
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class IntegrationTest {
+class It {
     @Test
     fun exampleTest(@Autowired webClient: WebTestClient) {
         webClient
