4bc71d96d7b1ad4f1305a96242c72d76741f5836
nyx <sakusennx@gmail.com>
2023-10-05 03:08:42 +0900

    検証:Profileを変更

--- src/test/kotlin/com/example/kotlinonspringboot/integration/actuator/It.kt
+++ src/test/kotlin/com/example/kotlinonspringboot/integration/actuator/It.kt
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.context.annotation.Profile
 import org.springframework.test.web.reactive.server.WebTestClient
 
+/**
+ * なぜかActionsで動作しない
+ */
+@Profile("local-test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class It {
     @Test
