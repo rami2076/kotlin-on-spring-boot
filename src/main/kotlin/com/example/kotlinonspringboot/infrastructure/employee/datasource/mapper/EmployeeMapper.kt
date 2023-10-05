1e5a65dfc127cbd2f2134ab34e2216e2bf06ba18
nyx <sakusennx@gmail.com>
2023-10-05 22:08:48 +0900

    EmployeeMapperの追加

---
+++ src/main/kotlin/com/example/kotlinonspringboot/infrastructure/employee/datasource/mapper/EmployeeMapper.kt
@@ -0,0 +1,30 @@
+package com.example.kotlinonspringboot.infrastructure.employee.datasource.mapper
+
+import com.example.kotlinonspringboot.infrastructure.employee.datasource.entity.EmployeeEntity
+import org.apache.ibatis.annotations.Insert
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.Update
+
+@Mapper
+interface EmployeeMapper {
+    @Select("SELECT EMPLOYEE_NUMBER, FULL_NAME,AGE,EMAIL_ADDRESS FROM EMPLOYEE ORDER BY EMPLOYEE_NUMBER DESC")
+    fun findAllSortedListByKeyDesc(): List<EmployeeEntity>
+
+    @Select(
+        """
+        SELECT EMPLOYEE_NUMBER, FULL_NAME,AGE,EMAIL_ADDRESS FROM EMPLOYEE
+         WHERE EMPLOYEE_NUMBER = #{employeeNumber}"""
+    )
+    fun findByKey(employeeNumber: Long): EmployeeEntity
+
+    @Insert("INSERT INTO EMPLOYEE ( FULL_NAME,AGE,EMAIL_ADDRESS) value(#{fullName},#{age},#{emailAddress})")
+    fun insert(fullName: String, age: Short, emailAddress: String): EmployeeEntity
+
+    @Update(
+        """
+        UPDATE EMPLOYEE SET FULL_NAME= #{fullName}, AGE= #{age}, EMAIL_ADDRESS = #{emailAddress}
+         WHERE EMPLOYEE_NUMBER = #{employeeNumber}"""
+    )
+    fun update(employeeNumber: Long, fullName: String, age: Short, emailAddress: String): EmployeeEntity
+}
